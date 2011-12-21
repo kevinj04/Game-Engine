b4f64fe27f0eb43b0e6ff7010a10d74cc9539279
Kevin Jenkins <kevinj04@gmail.com>
2011-12-21 14:15:32 -0500

    Initial Commit

---
+++ GameEngine/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  GameEngine
+//
+//  Created by Kevin Jenkins on 12/21/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ GameEngine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GameEngine
+//
+//  Created by Kevin Jenkins on 12/21/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
