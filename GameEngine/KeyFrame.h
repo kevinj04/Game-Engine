8a000fb72d991028fc1626aae5319fff678af6cd
Kevin Jenkins <kevinj04@gmail.com>
2012-01-20 11:19:49 -0500

    Created the KeyFrame object for managing graphic animations

---
+++ GameEngine/KeyFrame.h
@@ -0,0 +1,40 @@
+//
+//  KeyFrame.h
+//  GameEngine
+//
+//  Created by Kevin Jenkins on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const keyFrameTimePoint;
+extern NSString *const keyFrameSpriteFrame;
+extern NSString *const keyFrameSpritePosition;
+extern NSString *const keyFrameSpriteRotation;
+extern NSString *const keyFrameSpriteScale;
+
+@interface KeyFrame : NSObject {
+    
+    double timePoint;
+    
+    NSString *frame;
+    CGPoint position;
+    float rotation;
+    float scale;
+    
+}
+
+@property double timePoint;
+
+@property (nonatomic, retain) NSString *frame;
+@property CGPoint position;
+@property float rotation;
+@property float scale;
+
+- (id) initWithDictionary:(NSDictionary *) dictionary;
++ (id) frameWithDictionary:(NSDictionary *) dictionary;
+- (void) setupWithDictionary:(NSDictionary *) dictionary;
+- (void) dealloc;
+
+@end
---
+++ GameEngine/KeyFrame.m
@@ -0,0 +1,69 @@
+//
+//  KeyFrame.m
+//  GameEngine
+//
+//  Created by Kevin Jenkins on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KeyFrame.h"
+
+NSString *const keyFrameTimePoint = @"timePoint";
+NSString *const keyFrameSpriteFrame = @"frame";
+NSString *const keyFrameSpritePosition = @"position";
+NSString *const keyFrameSpriteRotation = @"rotation";
+NSString *const keyFrameSpriteScale = @"scale";
+
+@implementation KeyFrame
+
+@synthesize timePoint, frame, position, rotation, scale;
+
+- (id) initWithDictionary:(NSDictionary *) dictionary {
+    
+    if (( self = [super init] )) {
+        
+        [self setupWithDictionary:dictionary];
+        return self;
+        
+    } else {
+        return nil;
+    }
+    
+}
++ (id) frameWithDictionary:(NSDictionary *) dictionary {
+    return [[[KeyFrame alloc] initWithDictionary:dictionary] autorelease];
+}
+- (void) setupWithDictionary:(NSDictionary *) dictionary {
+    
+    timePoint = 0.0;
+    frame = keyFrameSpriteFrame;
+    position = CGPointMake(0.0,0.0);
+    rotation = 0.0;
+    scale = 1.0;    
+    
+    if ([dictionary objectForKey:keyFrameTimePoint] != nil) {
+        timePoint = [[dictionary objectForKey:keyFrameTimePoint] doubleValue];
+    }
+    
+    if ([dictionary objectForKey:keyFrameSpriteFrame] != nil) {
+        frame = [[dictionary objectForKey:keyFrameSpriteFrame] retain];
+    }
+    
+    if ([dictionary objectForKey:keyFrameSpritePosition] != nil) {
+        position = CGPointFromString([dictionary objectForKey:keyFrameSpritePosition]);
+    }
+    
+    if ([dictionary objectForKey:keyFrameSpriteRotation] != nil) {
+        rotation = [[dictionary objectForKey:keyFrameSpriteRotation] floatValue];
+    }
+    
+    if ([dictionary objectForKey:keyFrameSpriteScale] != nil) {
+        scale = [[dictionary objectForKey:keyFrameSpriteScale] floatValue];
+    }
+}
+- (void) dealloc {
+    if (frame != nil) { [frame release]; frame = nil; }
+    [super dealloc];
+}
+
+@end
