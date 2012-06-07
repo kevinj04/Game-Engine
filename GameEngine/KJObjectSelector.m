040fe75afbf2cb4756fe06c1b84df654929c79c4
Kevin Jenkins <kevinj04@gmail.com>
2012-06-07 18:55:03 -0400

    refactor ObjectSelector -- code clean

--- GameEngine/KJObjectSelector.m
+++ GameEngine/KJObjectSelector.m
@@ -12,14 +12,15 @@ static KJLevel *currentLevel;
 
 @implementation KJObjectSelector
 
-+ (void) setCurrentLevel:(KJLevel *) level {
++ (void) setCurrentLevel:(KJLevel *) level
+{
     currentLevel = level;
 }
++ (KJCommonGameObject *) getAnyObjectById:(NSString *) idString
+{
 
-+ (KJCommonGameObject *) getAnyObjectById:(NSString *) idString {
-    
     if (currentLevel == nil) return nil;
-    
+
     KJCommonGameObject *object = (KJCommonGameObject *)[[currentLevel objectDictionary] objectForKey:idString];
     KJLayer *layer = (KJLayer *)[[currentLevel layerDictionary] objectForKey:idString];
     KJCamera *camera = (KJCamera *)[[currentLevel cameraDictionary] objectForKey:idString];
@@ -34,42 +35,38 @@ static KJLevel *currentLevel;
         if (layer) return layer;
         if (camera) return camera;
     }
-    
+
     return nil;
 }
-
 + (NSObject<KJGameObjectProtocol> *) getObjectById:(NSString *)idString {
-    
+
     if (currentLevel == nil) { return  nil; }
-    
+
     return [[currentLevel objectDictionary] objectForKey:idString];
-    
 }
 + (NSArray *) allObjects
 {
     if (currentLevel == nil) { return nil; }
     return [[currentLevel objectDictionary] allValues];
 }
-
-+ (KJLayer *) getLayerById:(NSString *) idString 
++ (KJLayer *) getLayerById:(NSString *) idString
 {
     if (currentLevel == nil) { return nil; }
-    
+
     return [[currentLevel layerDictionary] objectForKey:idString];
 }
-+ (NSArray *) allLayers 
++ (NSArray *) allLayers
 {
     if (currentLevel == nil) { return nil; }
     return [[currentLevel layerDictionary] allValues];
 }
-
 + (KJCamera *) getCameraById:(NSString *)idString
 {
     if (currentLevel == nil) { return nil; }
-    
+
     return [[currentLevel cameraDictionary] objectForKey:idString];
 }
-+ (NSArray *) allCameras 
++ (NSArray *) allCameras
 {
     if (currentLevel == nil) { return nil; }
     return [[currentLevel cameraDictionary] allValues];
