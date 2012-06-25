c91d3580484e8ae68e92e5e02718d750482f5371
Kevin Jenkins <kevinj04@gmail.com>
2012-06-25 17:25:06 -0400

    refactor ObjectManagement Integration for clarity

--- UnitTests/ObjectManagementTests.m
+++ UnitTests/ObjectManagementTests.m
@@ -43,97 +43,155 @@
     [super tearDown];
 }
 
-- (KJCommonGameObject *) createObjectAndWindowContext
+- (void) createWindowContext
 {
     [self.level setActiveWindow:CGRectMake(0.0, 0.0, 480.0, 320.0)];
-
+}
+- (KJCommonGameObject *) createInactiveAndOutOfWindowObject
+{
     KJCommonGameObject *gameObject = [KJCommonGameObject object];
     [gameObject setPosition:CGPointMake(100.0, 100.0)];
     [gameObject setIsActive:NO];
     [self.objectManager addObject:gameObject];
-
+    [self.objectManager update:0.0];
     return gameObject;
 }
 
-- (void)testShouldActivateObjectsInWindow
+- (KJCommonGameObject *) createInactiveButInWindowObject
 {
-    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
+    KJCommonGameObject *gameObject = [KJCommonGameObject object];
+    [gameObject setPosition:CGPointMake(500.0, 100.0)];
+    [gameObject setIsActive:NO];
+    [self.objectManager addObject:gameObject];
+    [self.objectManager update:0.0];
+    return gameObject;
+}
 
-    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
-    STAssertTrue(0 == self.objectManager.impendingObjectsToActivate.count, @"Game object should not add to the active set yet.");
-
+- (KJCommonGameObject *) createActiveAndInWindowObject
+{
+    KJCommonGameObject *gameObject = [KJCommonGameObject object];
+    [gameObject setPosition:CGPointMake(100.0, 100.0)];
     [gameObject setIsActive:YES];
+    [self.objectManager addObject:gameObject];
+    [self.objectManager update:0.0];
+    return gameObject;
+}
 
-    STAssertTrue(0 == self.objectManager.inactiveObjects.count, @"Game objects should not be in the inactive set.");
-    STAssertTrue(1 == self.objectManager.impendingObjectsToActivate.count, @"Game object should be added to the impending objects to activate set");
+- (KJCommonGameObject *) createActiveButOutOfWindowObject
+{
+    KJCommonGameObject *gameObject = [KJCommonGameObject object];
+    [gameObject setPosition:CGPointMake(500.0, 100.0)];
+    [gameObject setIsActive:YES];
+    [self.objectManager addObject:gameObject];
+    [self.objectManager update:0.0];
+    return gameObject;
+}
+
+
+
+- (void)testShouldNotActivateInactiveObjects
+{
+    [self createWindowContext];
+    [self createInactiveButInWindowObject];
+    [self createInactiveAndOutOfWindowObject];
+    [self.objectManager update:0.0];
+
+    STAssertTrue(2 == self.objectManager.inactiveObjects.count &&
+                 0 == self.objectManager.impendingObjectsToActivate.count,
+                 @"Game objects should be in the inactive set.");
+}
+
+- (void)testActivatedObjectsShouldBePlacedInImpendingActivationSet
+{
+    [self createWindowContext];
+    KJCommonGameObject *gameObject1 = [self createInactiveButInWindowObject];
+    KJCommonGameObject *gameObject2 = [self createInactiveAndOutOfWindowObject];
+
+    [gameObject1 setIsActive:YES];
+    [gameObject2 setIsActive:YES];
+
+    STAssertTrue(2 == self.objectManager.impendingObjectsToActivate.count &&
+                 0 == self.objectManager.inactiveObjects.count,
+                 @"Game objects should not be in the inactive set.");
+}
+
+- (void)testShouldActivateImpendingObjectsOnUpdate
+{
+    [self createWindowContext];
+    KJCommonGameObject *gameObject1 = [self createInactiveButInWindowObject];
+    KJCommonGameObject *gameObject2 = [self createInactiveAndOutOfWindowObject];
+
+    [gameObject1 setIsActive:YES];
+    [gameObject2 setIsActive:YES];
 
     // update the object manager, this should move the gameObject into the active and on screen set
     [self.objectManager update:0.0];
 
-    STAssertTrue(0 == self.objectManager.impendingObjectsToActivate.count, @"Game object should leave the impending objects to add set");
-    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
+    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count &&
+                 1 == self.objectManager.activeButNotInWindowObjects.count &&
+                 0 == self.objectManager.impendingObjectsToActivate.count,
+                 @"Game object should leave the impending objects to add set");
 }
 
-- (void)testShouldSetToOutOfWindowObjectsThatExitWindow
+- (void)testShouldMoveActiveInWindowObjectsToOutOfWindowSet
 {
-    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
-    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
-
-    [gameObject setIsActive:YES];
-    [self.objectManager update:0.0];
-
-    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
-    STAssertTrue(0 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+    [self createWindowContext];
+    KJCommonGameObject *gameObject = [self createActiveAndInWindowObject];
 
     // move game object out of the active window
     gameObject.position = CGPointMake(500.0, 330.0);
     [self.objectManager update:0.0];
 
-    STAssertTrue(0 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
-    STAssertTrue(1 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
-
-    [gameObject setIsActive:NO];
-    STAssertTrue(0 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
-    STAssertTrue(1 == self.objectManager.impendingObjectsToDeactivate.count, @"Game object should now reside in the impending objects to deactivate set");
-
-    [self.objectManager update:0.0];
-
-    STAssertTrue(0 == self.objectManager.impendingObjectsToDeactivate.count, @"Game object should now reside in the impending objects to deactivate set");
-    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should now reside in the inactive set");
+    STAssertTrue(1 == self.objectManager.activeButNotInWindowObjects.count &&
+                 0 == self.objectManager.activeAndInWindowObjects.count,
+                 @"Game object should now reside in the activeAndInWindow set");
 }
 
 - (void)testShouldSetToOutOfWindowWhenWindowMovesAwayFromObject
 {
-    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
-    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+    [self createWindowContext];
+    [self createActiveAndInWindowObject];
 
-    [gameObject setIsActive:YES];
+    [self.level setActiveWindow:CGRectMake(-400.0, 0.0, 480.0, 320.0)];
     [self.objectManager update:0.0];
 
-    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
-    STAssertTrue(0 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+    STAssertTrue(1 == self.objectManager.activeButNotInWindowObjects.count &&
+                 0 == self.objectManager.activeAndInWindowObjects.count,
+                 @"Game object should now reside in the activeAndInWindow set");
+}
 
-    [self.level setActiveWindow:CGRectMake(0.0, 0.0, 50.0, 50.0)];
-    [self.objectManager update:0.0];
 
-    STAssertTrue(0 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
-    STAssertTrue(1 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+- (void)testShouldSetImpendingDeactivation
+{
+    [self createWindowContext];
+    KJCommonGameObject *gameObject1 = [self createActiveAndInWindowObject];
+    KJCommonGameObject *gameObject2 = [self createActiveButOutOfWindowObject];
 
+    [gameObject1 setIsActive:NO];
+    [gameObject2 setIsActive:NO];
+
+    STAssertTrue(2 == self.objectManager.impendingObjectsToDeactivate.count &&
+                 0 == self.objectManager.activeAndInWindowObjects.count,
+                 @"Game objects should be set to impending deactivation");
 }
 
 - (void)testShouldSetObjectInactiveWhenDeactivated
 {
-    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
-    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
 
-    [gameObject setIsActive:YES];
-    [self.objectManager update:0.0];
-    STAssertTrue(0 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+    KJCommonGameObject *gameObject1 = [self createActiveAndInWindowObject];
+    KJCommonGameObject *gameObject2 = [self createActiveButOutOfWindowObject];
 
-    [gameObject setIsActive:NO];
+    [gameObject1 setIsActive:NO];
+    [gameObject2 setIsActive:NO];
+
     [self.objectManager update:0.0];
-    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+    STAssertTrue(2 == self.objectManager.inactiveObjects.count &&
+                 0 == self.objectManager.activeAndInWindowObjects.count &&
+                 0 == self.objectManager.activeButNotInWindowObjects.count,
+                 @"Game objects should be in the inactive set.");
+
 
 }
+
 
 @end
