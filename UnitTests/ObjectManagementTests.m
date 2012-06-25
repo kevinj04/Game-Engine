e556ecbd5982feae23e86bb44b89e4eff593cba9
Kevin Jenkins <kevinj04@gmail.com>
2012-06-24 23:34:16 -0400

    add unit tests for ObjectManager

--- UnitTests/ObjectManagementTests.m
+++ UnitTests/ObjectManagementTests.m
@@ -7,17 +7,17 @@
 //
 
 #import "ObjectManagementTests.h"
-#import "KJObjectManager.h"
-#import "KJCommonGameObject.h"
-#import "KJLevel.h"
 
-@interface ObjectManagementTests : SenTestCase
+#import "kjEngine.h"
 
-@property (strong, retain) KJObjectManager* objectManager;
-@property (strong, retain) KJLevel* level;
-@property (strong, retain) KJCommonGameObject* gameObject;
+@interface ObjectManagementTests ()
+
+@property (nonatomic, retain) KJObjectManager* objectManager;
+@property (nonatomic, retain) KJLevel* level;
+@property (nonatomic, retain) KJCommonGameObject* gameObject;
 
 @end
+
 
 @implementation ObjectManagementTests
 
@@ -43,21 +43,97 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (KJCommonGameObject *) createObjectAndWindowContext
 {
-    STFail(@"Unit tests are not implemented yet in UnitTests");
+    [self.level setActiveWindow:CGRectMake(0.0, 0.0, 480.0, 320.0)];
+
+    KJCommonGameObject *gameObject = [KJCommonGameObject object];
+    [gameObject setPosition:CGPointMake(100.0, 100.0)];
+    [gameObject setIsActive:NO];
+    [self.objectManager addObject:gameObject];
+
+    return gameObject;
 }
 
 - (void)testShouldActivateObjectsInWindow
 {
-    [self.level setActiveWindow:CGRectMake(0.0, 0.0, 480.0, 320.0)];
-    
-    KJCommonGameObject *gameObject = [KJCommonGameObject object];
-    [gameObject setPosition:CGPointMake(100.0, 100.0)];
-    [self.level addObject:gameObject];
+    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
+
+    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+    STAssertTrue(0 == self.objectManager.impendingObjectsToActivate.count, @"Game object should not add to the active set yet.");
+
     [gameObject setIsActive:YES];
-    
-    
+
+    STAssertTrue(0 == self.objectManager.inactiveObjects.count, @"Game objects should not be in the inactive set.");
+    STAssertTrue(1 == self.objectManager.impendingObjectsToActivate.count, @"Game object should be added to the impending objects to activate set");
+
+    // update the object manager, this should move the gameObject into the active and on screen set
+    [self.objectManager update:0.0];
+
+    STAssertTrue(0 == self.objectManager.impendingObjectsToActivate.count, @"Game object should leave the impending objects to add set");
+    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
+}
+
+- (void)testShouldSetToOutOfWindowObjectsThatExitWindow
+{
+    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
+    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+
+    [gameObject setIsActive:YES];
+    [self.objectManager update:0.0];
+
+    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
+    STAssertTrue(0 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+
+    // move game object out of the active window
+    gameObject.position = CGPointMake(500.0, 330.0);
+    [self.objectManager update:0.0];
+
+    STAssertTrue(0 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
+    STAssertTrue(1 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+
+    [gameObject setIsActive:NO];
+    STAssertTrue(0 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+    STAssertTrue(1 == self.objectManager.impendingObjectsToDeactivate.count, @"Game object should now reside in the impending objects to deactivate set");
+
+    [self.objectManager update:0.0];
+
+    STAssertTrue(0 == self.objectManager.impendingObjectsToDeactivate.count, @"Game object should now reside in the impending objects to deactivate set");
+    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should now reside in the inactive set");
+}
+
+- (void)testShouldSetToOutOfWindowWhenWindowMovesAwayFromObject
+{
+    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
+    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+
+    [gameObject setIsActive:YES];
+    [self.objectManager update:0.0];
+
+    STAssertTrue(1 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
+    STAssertTrue(0 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+
+    [self.level setActiveWindow:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    [self.objectManager update:0.0];
+
+    STAssertTrue(0 == self.objectManager.activeAndInWindowObjects.count, @"Game object should now reside in the activeAndInWindow set");
+    STAssertTrue(1 == self.objectManager.activeButNotInWindowObjects.count, @"Game object should now reside in the impending objects to deactivate set");
+
+}
+
+- (void)testShouldSetObjectInactiveWhenDeactivated
+{
+    KJCommonGameObject *gameObject = [self createObjectAndWindowContext];
+    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+
+    [gameObject setIsActive:YES];
+    [self.objectManager update:0.0];
+    STAssertTrue(0 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+
+    [gameObject setIsActive:NO];
+    [self.objectManager update:0.0];
+    STAssertTrue(1 == self.objectManager.inactiveObjects.count, @"Game object should be in the inactive set.");
+
 }
 
 @end
--- UnitTests/UnitTests.m
+++ UnitTests/UnitTests.m
@@ -13,20 +13,16 @@
 - (void)setUp
 {
     [super setUp];
-    
+
     // Set-up code here.
 }
 
 - (void)tearDown
 {
     // Tear-down code here.
-    
+
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in UnitTests");
-}
 
 @end
