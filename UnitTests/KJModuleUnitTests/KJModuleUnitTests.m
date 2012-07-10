41fa6b85c219086fe478f6446e218ea57438ee24
Kevin Jenkins <kevinj04@gmail.com>
2012-07-10 13:51:52 -0400

    [#32181085] add test file for KJModule and write test stubs

---
+++ UnitTests/KJModuleUnitTests/KJModuleUnitTests.m
@@ -0,0 +1,55 @@
+//
+//  KJModuleUnitTests.m
+//  GameEngine
+//
+//  Created by Kevin Jenkins on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KJModuleUnitTests.h"
+
+@implementation KJModuleUnitTests
+
+#pragma mark - Test Life Cycle
+- (void) setUp
+{
+    [super setUp];
+}
+- (void) tearDown
+{
+    [super tearDown];
+}
+
+#pragma mark - Creation Tests
+- (void) testShouldCreateDefaultObject
+{
+    STFail(@"Test not written.");
+}
+
+- (void) testShouldCreateModuleFromDictionary
+{
+    STFail(@"Test not written.");
+}
+
+- (void) testShouldCreateModuleForObjectWithModuleDictionary
+{
+    STFail(@"Test not written.");
+}
+
+- (void) testShouldIncrementDefaultId
+{
+    STFail(@"Test not written.");
+}
+
+#pragma mark - Setters and Getters
+- (void) testShouldAttachToParent
+{
+    STFail(@"Test not written.");
+}
+
+#pragma mark - Detach
+- (void) testShouldDetachFromEverything
+{
+    STFail(@"Test not written.");
+}
+@end
